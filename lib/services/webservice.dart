25f96d6973880a1b2459c732f0bef19a4c61805b
jerry hung <jerry110160@gmail.com>
2020-06-02 12:24:32 -0400

    add desc and canonical

--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -19,24 +19,9 @@ class Webservice {
           }));
 
       var document = parse(response.toString());
-      var resultUri = response.redirects.length > 0
-          ? response.redirects[0].location
-          : response.realUri.toString();
-      var redirectCode = response.redirects.length > 0
-          ? response.redirects[0].statusCode
-          : 200;
-
-      var h1 = document.getElementsByTagName('h1').isNotEmpty
-          ? document.getElementsByTagName('h1').first.text
-          : "";
-      Element title = document.getElementsByTagName('title').first;
-
-      movies.add(Movie(
-          title: resultUri.toString(), description: redirectCode.toString()));
-      movies.add(Movie(title: 'h1', description: h1.trim()));
-      movies.add(Movie(title: 'title', description: title.text.trim()));
+      _AddSuccessResult(response, document, movies);
     } catch (e) {
-      movies.add(Movie(title: 'Wrong response', description: e.toString()));
+      _AddErrorResult(movies, e);
     }
 
     var completer = new Completer<List<Movie>>();
@@ -44,5 +29,39 @@ class Webservice {
     completer.complete(movies);
 
     return completer.future;
+  }
+
+  void _AddSuccessResult(
+      Response response, Document document, List<Movie> movies) {
+    const desc_selector = 'meta[name="description"]';
+    const canonical_selector = 'link[rel="canonical"]';
+    var resultUri = response.redirects.length > 0
+        ? response.redirects[0].location
+        : response.realUri.toString();
+    var redirectCode =
+        response.redirects.length > 0 ? response.redirects[0].statusCode : 200;
+
+    var h1 = document.getElementsByTagName('h1').isNotEmpty
+        ? document.getElementsByTagName('h1').first.text
+        : "";
+    var metaDescription = document.querySelectorAll(desc_selector).isNotEmpty
+        ? document.querySelectorAll(desc_selector).first.attributes['content']
+        : "";
+    var canonical = document.querySelectorAll(canonical_selector).isNotEmpty
+        ? document.querySelectorAll(canonical_selector).first.attributes['href']
+        : "";
+    Element title = document.getElementsByTagName('title').first;
+
+    movies.add(Movie(
+        title: resultUri.toString(), description: redirectCode.toString()));
+    movies.add(Movie(title: 'h1', description: h1.trim()));
+    movies.add(Movie(title: 'title', description: title.text.trim()));
+    movies
+        .add(Movie(title: 'description', description: metaDescription.trim()));
+    movies.add(Movie(title: 'canonical', description: canonical.trim()));
+  }
+
+  void _AddErrorResult(List<Movie> movies, e) {
+    movies.add(Movie(title: 'Wrong response', description: e.toString()));
   }
 }
